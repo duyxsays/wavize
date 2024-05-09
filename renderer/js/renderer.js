bf41dc1106c2ad8e6e096e73048b465a3c646767
Simon Andersen <sandersen98@live.dk>
2024-05-09 12:18:03 +0200

    clean up

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,13 +1,6 @@
-const form = document.querySelector('#img-form');
-const dirInput = document.querySelector('#dir-input');
-const outputPath = document.querySelector('#output-path');
-const filename = document.querySelector('#filename');
-const filePathElement = document.getElementById('filePath');
-const destPathElement = document.getElementById('destPath');
-const list = document.getElementById('list');
+const ws = require('wavesurfer.js');
 
 // New elements
-const btn = document.getElementById('btn');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
@@ -20,43 +13,6 @@ var folderSelection = document.getElementById("folderSelection");
 var selectedFoldersList = document.getElementById("selectedFoldersList");
 var afterLoading = document.getElementById("afterLoading");
 var loadingContainer = document.getElementById("loadingDiv");
-
-function isFileAudio(file) {
-  const acceptedAudioTypes = ['audio/wav', 'audio/mp3'];
-  return file && acceptedAudioTypes.includes(file['type']);
-}
-
-function loadDirectory(directory) {
-  const files = fs.readdirSync(directory);
-  files.map(item => {
-    let li = document.createElement('li');
-    li.innerText = item;
-    list.appendChild(li);
-  });
-  console.log(files);
-}
-
-// Resize image
-function resizeImage(e) {
-  e.preventDefault();
-
-  if (!audio.files[0]) {
-    alertError('Please upload an image');
-    return;
-  }
-
-  if (widthInput.value === '' || heightInput.value === '') {
-    alertError('Please enter a width and height');
-    return;
-  }
-
-  // Electron adds a bunch of extra properties to the file object including the path
-  const imgPath = audio.files[0].path;
-
-  ipcRenderer.send('audio:predict', {
-    imgPath,
-  });
-}
 
 // When done, show message
 ipcRenderer.on('image:done', () =>
@@ -97,29 +53,10 @@ function sendRequest() {
   });
 }
 
-// form.addEventListener('submit', resizeImage);
-/*
-btn.addEventListener('click', async () => {
-  const data = await window.ipcRenderer.request('hf:rq', '/Users/duyx/Code/Classify/data/evaluate/version2.0/808/JETSONMADE_808_class2_C.wav');
-  
-  ipcRenderer.send('copy:file', data);
-
-  // const prediction = findClosestTo1(data);
-  // filePathElement.innerText = prediction.label;
-  
-  /*
-  moveFile(
-    '/Users/duyx/Code/Classify/data/evaluate/version2.0/808/JETSONMADE_808_class2_C.wav',
-    '/Users/duyx/Code/electron-app/samples/'
-  );
-
-});
-*/
-
 btn2.addEventListener('click', async () => {
   const filePath = await window.ipcRenderer.request('dialog:openDirectory', 'samplesPath');
   console.log(filePath);
-  // loadDirectory(filePath);
+  
   if (filePath != null) 
   {
     sampleFolderLabel.textContent = filePath;
@@ -154,7 +91,7 @@ function simulateLoading()
     afterLoading.style.display = "block";
     btn5.style.display = "block";
     showSelectedFolders();
-  }, 3000); // Adjust the time as needed
+  }, 3000); 
 }
 
 btn5.addEventListener('click', async () => {
@@ -181,8 +118,8 @@ ipcRenderer.on('responses:handled', (event, data) => {
   showSelectedFolders();
 });
 
+// Handle the received data
 ipcRenderer.on('requested:samples', (event, data) => {
-  // Handle the received data
   selectedFoldersList.innerHTML = ''; // Clear previous content
 
   for (let i = 0; i < data.length; i++)
@@ -203,45 +140,3 @@ ipcRenderer.on('requested:samples', (event, data) => {
     }
   }
 });
-
-/* -------- Redacted
-// dirInput.addEventListener('change', loadAudio);
-
-function loadAudio(e) {
-  const file = e.target.files[0];
-
-  // Check if file is an image
-  if (!isFileAudio(file)) {
-    alertError('Please select an image');
-    return;
-  }
-  console.log('test');
-
-  form.style.display = 'block';
-  filename.innerHTML = audio.files[0].name;
-  outputPath.innerText = path.join(os.homedir(), 'imageresizer');
-}
-
-*/
-
-/* ------- Title bar things
-// Minimize button event listener
-document.getElementById('titlebar-minimize-button').addEventListener('click', function() {
-  require('electron').remote.getCurrentWindow().minimize();
-});
-
-// Maximize button event listener
-document.getElementById('titlebar-maximize-button').addEventListener('click', function() {
-  var window = require('electron').remote.getCurrentWindow();
-  if (!window.isMaximized()) {
-    window.maximize();
-  } else {
-    window.unmaximize();
-  }
-});
-
-// Close button event listener
-document.getElementById('titlebar-close-button').addEventListener('click', function() {
-  require('electron').remote.getCurrentWindow().close();
-});
-*/
